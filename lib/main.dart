f3a90c6bbcb43f6fdc80d2a5cf44455c1a584bb7
shichi <n.shichi18@gmail.com>
2021-03-23 07:19:19 +0900

    [add] add TabView & Radio URL

--- lib/main.dart
+++ lib/main.dart
@@ -21,27 +21,76 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class WebViewPage extends StatefulWidget {
-  @override
-  _WebViewPageState createState() => _WebViewPageState();
-}
-
-class _WebViewPageState extends State<WebViewPage> {
-  @override
-  void initState() {
-    super.initState();
-
-    // Enable hybrid composition.
-    if (Platform.isAndroid) WebView.platform = SurfaceAndroidWebView();
-  }
-
+class WebViewPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Scaffold(
-        body: WebView(
-          initialUrl: 'https://www.google.com/',
-          javascriptMode: JavascriptMode.unrestricted,
+    if (Platform.isAndroid) WebView.platform = SurfaceAndroidWebView();
+
+    return MaterialApp(
+      home: DefaultTabController(
+        length: 4,
+        child: Scaffold(
+          appBar: AppBar(
+            flexibleSpace: Column(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                TabBar(
+                  tabs: [
+                    Tab(icon: Icon(Icons.radio)),
+                    Tab(
+                      text: '超A&G',
+                    ),
+                    Tab(
+                      text: '音泉',
+                    ),
+                    Tab(
+                      text: '響',
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+          body: TabBarView(
+            children: [
+              Center(
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Text(
+                        'ラジオ まとめ',
+                        style: TextStyle(fontSize: 28),
+                      ),
+                    ),
+                    Column(
+                      mainAxisSize: MainAxisSize.min,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text('超A&G'),
+                        Text('音泉'),
+                        Text('響'),
+                      ],
+                    ),
+
+                  ],
+                ),
+              ),
+              WebView(
+                initialUrl: 'https://www.uniqueradio.jp/agplayer5/player.php',
+                javascriptMode: JavascriptMode.unrestricted,
+              ),
+              WebView(
+                initialUrl: 'https://www.onsen.ag/',
+                javascriptMode: JavascriptMode.unrestricted,
+              ),
+              WebView(
+                initialUrl: 'https://hibiki-radio.jp/',
+                javascriptMode: JavascriptMode.unrestricted,
+              ),
+            ],
+          ),
         ),
       ),
     );
