1a4253902569f46dd5f9c132ac7df36655859f03
shichi <n.shichi18@gmail.com>
2021-06-09 01:28:51 +0900

    [add] add radio url

--- lib/radio_data.dart
+++ lib/radio_data.dart
@@ -2,6 +2,8 @@ final radioDataList = [
   RadioData('超!A&G+', 'https://www.uniqueradio.jp/agplayer5/player.php'),
   RadioData('インターネットラジオステーション＜音泉＞', 'https://www.onsen.ag/'),
   RadioData('響 -HiBiKi Radio Station-', 'https://hibiki-radio.jp/'),
+  RadioData('radiko', 'https://radiko.jp/'),
+  RadioData('animate Times', 'https://www.animatetimes.com/radio/'),
 ];
 
 class RadioData {
